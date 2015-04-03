7c547e96cc31d3f1c382472a3c7d85ea6e9fb293
Ernesto Nobmann Chavez <nobmann88@gmail.com>
2015-04-03 15:04:04 -0500

    Mejora el diseño del login
    - Se agrega div para mostrar errores en el login
    - Se cambia lógica y se utiliza formularios para el login
    - Se eliminan urls innecesarias
    - Se cambia lógica en views.py

--- vhs_web/account/forms.py
+++ vhs_web/account/forms.py
@@ -12,3 +12,7 @@ class RegisterForm(forms.Form):
 	money_administration = forms.BooleanField(required=False)
 	report = forms.BooleanField(required=False)
 	social_network = forms.BooleanField(required=False)
+
+class LoginForm(forms.Form):
+	username = forms.CharField(max_length=100, required = True)
+	password = forms.CharField(widget=forms.PasswordInput, required = True)
