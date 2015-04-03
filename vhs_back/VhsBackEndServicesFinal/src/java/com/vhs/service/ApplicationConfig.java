5ba389e01ecdaad592cd6bbd6124a5c2c59638f0
alex.chacon <alex.chacon@software-colombia.com>
2015-04-02 21:03:43 -0500

    Base line project

---
+++ vhs_back/VhsBackEndServicesFinal/src/java/com/vhs/service/ApplicationConfig.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vhs.service;
+
+import java.util.Set;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author andresvargas
+ */
+@javax.ws.rs.ApplicationPath("webresources")
+public class ApplicationConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<Class<?>>();
+        addRestResourceClasses(resources);
+        return resources;
+    }
+
+    /**
+     * Do not modify addRestResourceClasses() method.
+     * It is automatically populated with
+     * all resources defined in the project.
+     * If required, comment out calling this method in getClasses().
+     */
+    private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(com.vhs.service.VhsCategoryFacadeREST.class);
+        resources.add(com.vhs.service.VhsCityFacadeREST.class);
+        resources.add(com.vhs.service.VhsCountryFacadeREST.class);
+        resources.add(com.vhs.service.VhsSpecialOfferFacadeREST.class);
+        resources.add(com.vhs.service.VhsUserFacadeREST.class);
+    }
+    
+}
