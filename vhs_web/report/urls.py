6af05a6f97e2e2471f807f8cf5371848e99526fa
Ernesto Nobmann Chavez <nobmann88@gmail.com>
2015-05-01 12:55:34 -0500

    Reporte - Location Period
    - Se realiza reporte el cual muestra las ventas de los mejores paquetes por periodo

--- vhs_web/report/urls.py
+++ vhs_web/report/urls.py
@@ -7,5 +7,5 @@ urlpatterns = patterns('',
 	url(r'^ratingPackage/$', views.rating_report_package),
 	url(r'^ratingLocation/$', views.rating_report_location),
 	url(r'^salesLocation/$', views.sales_report_location),
-
+	url(r'^salesPeriod/$', views.sales_report_period),
 )
--- vhs_web/report/views.py
+++ vhs_web/report/views.py
@@ -208,8 +208,6 @@ def sales_report_location(request):
 				for infoRating in root:
 					listado_productos[infoRating[1].text] = float(infoRating[0].text)
 
-				print listado_productos
-
 		except Exception, e:
 			raise e
 
@@ -227,6 +225,82 @@ def sales_report_location(request):
 		return render(request, 'report/sales_report_location.html', {'mejores_ciudades': mejores_ciudades, 
 			'mejores_ventas': mejores_ventas, 
 			'peores_ciudades': peores_ciudades,
+			'peores_ventas': peores_ventas,
+			'fecha': str(date) })
+		
+	else:
+		form = LoginForm(request.POST)
+		form.add_error(None, "Please enter your username and password")
+		return render(request, 'account/login.html', {'form': form})
+
+def sales_report_period(request):
+
+	if esta_autenticado(request):
+
+		fecha_inicio = ''
+		fecha_fin = ''
+
+		if request.method == 'POST':
+
+			form = RatingForm(request.POST)
+
+			if form.is_valid():
+				fecha_inicio = form.cleaned_data['start_date']
+				fecha_fin = form.cleaned_data['end_date']
+				
+		# date = {'date': '03/04/2015 11:58:00 PM'}
+		date = datetime.today().strftime('%d/%m/%Y %I:%M:%S %p')
+
+		# ToDo: El metodo puede que cambie dependiendo del servicio
+		
+		url = 'http://jbossasvhsbackendservices-vhstourism.rhcloud.com/VhsBackEndServices/webresources'
+		method = 'vhsoffersale'
+		username = request.session['mail_usuario'] #"andresvargasr@gmail.com"
+
+		# Se convierten a Date, esto se debe mejorar
+
+		if fecha_inicio == '':
+			fecha_fin = datetime.today()
+			fecha_inicio = fecha_fin - timedelta(days=10)
+		else:
+			fecha_inicio = datetime.strptime(fecha_inicio, '%d/%m/%Y')
+			fecha_fin = datetime.strptime(fecha_fin, '%d/%m/%Y')
+	
+		# Se convierten a String
+
+		fecha_fin = fecha_fin.strftime('%d%m%Y')
+		fecha_inicio = fecha_inicio.strftime('%d%m%Y')
+
+		response = requests.get('{0}/{1}/{2}/{3}/{4}/product'.format(url, method, username, fecha_inicio, fecha_fin))
+		
+		try:
+			if response.status_code == 200:
+				root =  ElementTree.XML(response.text)
+
+				
+				
+				listado_productos = {}
+
+				for infoRating in root:
+					listado_productos[infoRating[1].text] = float(infoRating[0].text)
+
+		except Exception, e:
+			raise e
+
+		mejores_10 = sorted(listado_productos.iteritems(), key=lambda (k, v): (-v, k))[:10]
+		peores_10 = sorted(listado_productos.iteritems(), key=lambda (k, v): (v, k))[:10]
+		
+		mejores_10 = dict(mejores_10)
+		mejores_productos = mejores_10.keys()
+		mejores_ventas = mejores_10.values()
+
+		peores_10 = dict(peores_10)
+		peores_productos = peores_10.keys()
+		peores_ventas = peores_10.values()
+
+		return render(request, 'report/sales_report_period.html', {'mejores_productos': mejores_productos, 
+			'mejores_ventas': mejores_ventas, 
+			'peores_productos': peores_productos,
 			'peores_ventas': peores_ventas,
 			'fecha': str(date) })
 		
