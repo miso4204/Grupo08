0e63cbf46e7ae36fb5a1f0045fb3038f48d1bca2
ifgs1 <ifgs1@hotmail.com>
2015-05-10 19:26:01 -0500

    Additional Values Aded for each vhsspecialoffer

---
+++ vhs_ios/VHSMarketPlace/AdditionalValue.m
@@ -0,0 +1,13 @@
+//
+//  AdditionalValue.m
+//  VHSMarketPlace
+//
+//  Created by Ivan F Garcia S on 5/10/15.
+//  Copyright (c) 2015 TSFactory. All rights reserved.
+//
+
+#import "AdditionalValue.h"
+
+@implementation AdditionalValue
+
+@end
