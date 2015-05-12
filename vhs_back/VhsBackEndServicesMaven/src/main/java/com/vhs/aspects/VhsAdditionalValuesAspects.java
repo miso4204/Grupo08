5556c505a6ebedab9b3c1de66607a2c1cfb7b8e3
alexchacon <alex.chacon@gmail.com>
2015-05-12 02:10:31 -0500

    Aspect J Added
    Aspect J Added

---
+++ vhs_back/VhsBackEndServicesMaven/src/main/java/com/vhs/aspects/VhsAdditionalValuesAspects.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vhs.aspects;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+/**
+ * Additional values aspects definition.
+ *
+ * @author Andres Vargas (ja.vargas147@uniandes.edu.co)
+ * @author Alex Vicente ChacOn JimEnez (av.chacon10@uniandes.edu.co)
+ */
+@Aspect
+public class VhsAdditionalValuesAspects
+{
+    @Around("execution(@com.vhs.annotations.VHSFeature * com.vhs.service.AdditionalValuesFacadeREST.findBySpecialOffer(..))")
+    public Object adviceFindBySpecialOffer(ProceedingJoinPoint pjp) throws Throwable
+    {
+        Logger.getLogger(VhsAdditionalValuesAspects.class.getName()).log(Level.INFO, "Feature interceptado: ExtendedProductDescription");
+        return null;
+    }
+}
