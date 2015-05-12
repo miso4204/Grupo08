19df416ddae4307c19e1fa7ae61333a410d4aab2
Ernesto Nobmann Chavez <nobmann88@gmail.com>
2015-05-11 23:28:58 -0500

    Cambio de Index
    - Se realiza cambio conectándose al servicio para que muestre solo las really really ofertas speciales xD

--- vhs_web/account/views.py
+++ vhs_web/account/views.py
@@ -54,7 +54,50 @@ def auth_view(request):
 	return render(request, 'account/login.html', {'form': form})
 
 def index(request):
+
+	productos = []
+
 	if esta_autenticado(request):
+
+		#Realizar el llamado al método REST para obtener la información
+
+		try:
+			url = settings.CUSTOM_BASE_URL + '/VhsBackEndServices/webresources'
+			method = 'vhsspecialoffer/special'
+			
+			response = requests.get('{0}/{1}/'.format(url, method))
+
+			if response.status_code == 200:
+				tree = xmltodict.parse(response.text)['collection']['vhsSpecialOffer']
+
+				for element in tree:
+					producto = {}
+					try:
+						# Defaults
+
+						producto['name'] = 'No Name'
+						producto['description'] = 'No Description'
+						producto['price'] = 0
+						producto['image'] = 'http://www.ikea.com/ms/media/seorange/20151/20151_besr01a_hemnes_PH121213.jpg'
+
+						# Defining...
+
+						producto['name'] = element['shortName']
+						producto['description'] = element['description']
+						producto['price'] = element['price']
+						producto['image'] = element['mainImageUrl']
+
+					except Exception, e:
+						raise e
+					finally:
+						productos.append(producto)
+				
+				print productos	
+
+		except Exception, e:
+			raise e
+		return render(request, 'product/productos.html', { 'response': True,  'productos': productos })
+
 		return render_to_response('account/index.html')
 	else:
 		form = LoginForm(request.POST)
